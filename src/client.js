e1e3658897285cb2ad7a3c2ea17c8c3cb5e4fdcf
Jeff Jagoda <jeff.jagoda@lifeomic.com>
2018-07-19 09:18:46 -0400

    Retry failed requests

--- src/client.js
+++ src/client.js
@@ -1,6 +1,7 @@
 const app = require('./app');
 const assert = require('assert');
 const axios = require('axios');
+const axiosRetry = require('axios-retry');
 
 module.exports = ({ token }) => {
   assert(token, 'An access token is required');
@@ -9,6 +10,8 @@ module.exports = ({ token }) => {
     baseURL: 'https://api.bitrise.io/v0.1',
     headers: { Authorization: `token ${token}` }
   });
+
+  axiosRetry(client, { retryDelay: axiosRetry.exponentialDelay });
 
   const instance = {};
   instance.app = ({ slug }) => app({ client, slug });
--- test/client.test.js
+++ test/client.test.js
@@ -1,17 +1,27 @@
 const axios = require('axios');
 const client = require('..');
+const nock = require('nock');
 const sinon = require('sinon');
 const test = require('ava');
 const uuid = require('uuid/v4');
 
-test('an api token is required', (test) => {
+test.beforeEach(() => {
+  nock.disableNetConnect();
+});
+
+test.afterEach(() => {
+  nock.enableNetConnect();
+  nock.cleanAll();
+});
+
+test.serial('an api token is required', (test) => {
   test.throws(() => client());
   test.throws(() => client({}));
   test.truthy(client({ token: uuid() }));
 });
 
 test.serial('a client is authenticated', (test) => {
-  const create = sinon.stub(axios, 'create');
+  const create = sinon.spy(axios, 'create');
   const token = uuid();
 
   try {
@@ -31,7 +41,9 @@ test.serial('a client is authenticated', (test) => {
 });
 
 test.serial('a client can create an app', async (test) => {
-  const axiosInstance = { post: sinon.stub().rejects(new Error('fake')) };
+  const axiosInstance = axios.create();
+  sinon.stub(axiosInstance, 'post').rejects(new Error('fake'));
+
   const create = sinon.stub(axios, 'create').returns(axiosInstance);
   const slug = uuid();
   const token = uuid();
@@ -47,6 +59,49 @@ test.serial('a client can create an app', async (test) => {
     }
 
     sinon.assert.calledOnce(axiosInstance.post);
+  } finally {
+    create.restore();
+  }
+});
+
+test.serial('a client retries requests', async (test) => {
+  const create = sinon.spy(axios, 'create');
+  const token = uuid();
+
+  nock('http://localhost')
+    .get('/error').replyWithError('boom')
+    .get('/error').reply(500)
+    .get('/error').reply(200, 'success!');
+
+  try {
+    const instance = client({ token });
+    test.truthy(instance);
+
+    sinon.assert.calledOnce(create);
+    const httpClient = create.firstCall.returnValue;
+    const response = await httpClient.get('http://localhost/error');
+    test.is(response.data, 'success!');
+  } finally {
+    create.restore();
+  }
+});
+
+test.serial('a client gives up after too many retries', async (test) => {
+  const create = sinon.spy(axios, 'create');
+  const token = uuid();
+
+  nock('http://localhost')
+    .get('/error').replyWithError('boom')
+    .get('/error').thrice().reply(500);
+
+  try {
+    const instance = client({ token });
+    test.truthy(instance);
+
+    sinon.assert.calledOnce(create);
+    const httpClient = create.firstCall.returnValue;
+    const { response } = await test.throws(httpClient.get('http://localhost/error'));
+    test.is(response.status, 500);
   } finally {
     create.restore();
   }
